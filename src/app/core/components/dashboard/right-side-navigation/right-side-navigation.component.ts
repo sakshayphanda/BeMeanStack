ba925371c2f3b863f86815cc130bfd3dc4525e7b
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-06-12 02:50:30 +0530

    update

--- src/app/core/components/dashboard/right-side-navigation/right-side-navigation.component.ts
+++ src/app/core/components/dashboard/right-side-navigation/right-side-navigation.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { IUserInfo } from 'src/app/shared/models/interfaces/authenticate.interface';
+import { currentUser } from 'src/app/store/selectors/auth.selector';
+import { Store } from '@ngrx/store';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppState } from 'src/app/store/reducers';
 
 @Component({
   selector: 'app-right-side-navigation',
@@ -7,10 +12,48 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RightSideNavigationComponent implements OnInit {
-
-  constructor() { }
+  list = [];
+  selectedType = 'friends';
+  currentUser: IUserInfo;
+  types = [{
+    id: 'friends',
+    label: 'Friends'
+  }, {
+    id: 'friendRequests',
+    label: 'Friend Requests'
+  }];
+  constructor(
+    private store: Store<AppState>,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.store.select(currentUser).subscribe(
+      (user: IUserInfo) => {
+        if (user) {
+          this.currentUser = user;
+          this.list = JSON.parse(JSON.stringify(this.currentUser['friends']));
+        }
+      }
+    );
+  }
+
+  typeChanged(id) {
+    this.selectedType = id;
+    // this.router.navigate([id]);
+    this.list = JSON.parse(JSON.stringify(this.currentUser[this.selectedType]));
+  }
+
+
+
+  unfriend(user) {
+    // this.store.dispatch(new UnfriendApi({
+    //   to: user[`_id`],
+    //   from: this.currentUser[`_id`]
+    // }));
+    // const index = this.list.findIndex(element => element._id === user._id);
+    // this.list.splice(index, 1);
   }
 
 }
