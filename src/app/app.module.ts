7cc7e7f9467b32a0450e8519c63ad47e84fd2a83
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-06-29 01:35:23 +0530

    updated schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,26 @@ import { reducers, metaReducers } from './store/reducers';
 import { AuthEffects } from './store/effects/authenticaton/auth.effects';
 import { UsersEffects } from './store/effects/users/users.effect';
 import { PostsEffects } from './store/effects/posts/posts.effect';
+import { DragComponent } from './shared/schemantics/drag/drag.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { TreeComponent } from './shared/schemantics/tree/tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DashComponent } from './shared/schemantics/dash/dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
+import { TableComponent } from './shared/schemantics/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { AddressFormComponent } from './shared/schemantics/address-form/address-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 const config = {
@@ -29,7 +49,12 @@ const config = {
 firebase.initializeApp(config);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragComponent,
+    TreeComponent,
+    DashComponent,
+    TableComponent,
+    AddressFormComponent
   ],
   imports: [
     CoreModule,
@@ -40,7 +65,22 @@ firebase.initializeApp(config);
     }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
-    EffectsModule.forRoot([AuthEffects, UsersEffects, PostsEffects])
+    EffectsModule.forRoot([AuthEffects, UsersEffects, PostsEffects]),
+    DragDropModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    LayoutModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
